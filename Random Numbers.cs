cfa7eeeac0141785e106d5f735fa13c9a2a2f17d
RangerDonSD <107903989+RangerDonSD@users.noreply.github.com>
2022-06-21 11:01:49 +0700

    Add files via upload

---
+++ Random Numbers.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Students
+ * Дата: 21.06.2022
+ * Время: 10:51
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+
+namespace ваывдаыва
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			int value = rnd.Next();
+			Console.WriteLine(value);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
